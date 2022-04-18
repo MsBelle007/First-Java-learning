2d38f27cc98ad23bd2f5b2cf7d603486310d8198
MsBelle007 <101517000+MsBelle007@users.noreply.github.com>
2022-04-18 14:08:57 +0100

    Add files via upload

---
+++ JavaScript Learning/app.js
@@ -0,0 +1,17 @@
+// alert("hello World");
+// h2.sea
+// // undefined means nothing was returned from your code. console.table({a:1, b:2});
+// console.log("hello World");
+30 + 20 + 30;
+console.log(30 + 20 + 30);
+let country = "Nigeria";
+let continent = "Africa";
+console.log(country);
+const now = 2048;
+const ageDami = now - 208;
+const ageSea = now - 209;
+console.log(ageDami * 2, ageSea / 15, 2 ** 3);
+// 2**3 2 raised to power 3 i.e 2*2*2
+// joining strings
+console.log(country + " " + continent);
+// += means add to the current variable.++ and -- mean variabe +1 and variable -1
